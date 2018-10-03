7c47c471f71301ca29353e44ce7e89276a342581
Jordan Mor <jordanmor@gmail.com>
2018-10-03 04:55:11 -0400

    Connect mongoose to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const genres = require('./routes/genres');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', err => console.error('connection error:', err));
+db.once('open', () => console.log('db connection successful'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
