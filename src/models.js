91235cc4368e9c2ece84a07c78a809f9d68bbfe6
Jordan Mor <jordanmor@gmail.com>
2018-10-03 11:10:25 -0400

    Improve project models

--- src/models.js
+++ src/models.js
@@ -2,24 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Genre name is required'],
-        minlength: 3,
-        maxLength: 50
-    }
+  name: {
+    type: String,
+    required: [true, 'Genre name is required'],
+    minlength: 3,
+    maxLength: 50
+  }
 });
 
 const CustomerSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Customer name is required']
+  name: {
+    type: String,
+    required: [true, 'Customer name is required']
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function(num) {
+        return /\d{3}-\d{3}-\d{4}/.test(num);
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    phone: String,
-    isGold: {
-        type: Boolean,
-        default: false
-    }
+    required: [true, 'User phone number required']
+  },
+  isGold: {
+    type: Boolean,
+    default: false
+  }
 });
 
 const Genre = mongoose.model('Genre', GenreSchema);
