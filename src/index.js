a36480cd465cb6855ad4734d6b35c3ae92f92628
Jordan Mor <jordanmor@gmail.com>
2018-10-03 18:53:25 -0400

    Add user model and routes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const users = require('./routes/users');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
@@ -13,6 +14,7 @@ db.on('error', err => console.error('connection error:', err));
 db.once('open', () => console.log('db connection successful'));
 
 app.use(express.json());
+app.use('/api/users', users);
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+module.exports.User = User;
+module.exports.validate = validateUser;
