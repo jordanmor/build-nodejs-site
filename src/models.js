1d1505b9ed5f173c77ac9aa24f03666374e3ed5a
Jordan Mor <jordanmor@gmail.com>
2018-10-03 05:24:09 -0400

    Create Genre schema and model

---
+++ src/models.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Genre name is required'],
+        minlength: 5,
+        maxLength: 50
+    }
+});
+
+const Genre = mongoose.model('Genre', GenreSchema);
+
+module.exports.Genre = Genre;
